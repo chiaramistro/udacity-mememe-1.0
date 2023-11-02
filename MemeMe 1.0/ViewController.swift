c0b5d97129b28613e213f68e88875eb96adc3bc1
Chiara Mistrorigo <c.mistrorigo@reply.it>
2023-11-02 12:19:28 +0100

    Open picker for photo (gallery and camera)

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var shootButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear()")
+        
+        // The Camera button is disabled when app is run on devices without a camera, such as the simulator.
+        shootButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    
     // MARK: Actions
 
     @IBAction func pickFromGallery(_ sender: Any) {
         print("Pick from gallery")
+        openPicker(source: .photoLibrary)
     }
     
+    // The app displays the camera when the Camera button is pressed on a phone.
     @IBAction func shootFromCamera(_ sender: Any) {
         print("Shoot from camera")
+        openPicker(source: .camera)
     }
 
     
+    func openPicker(source: UIImagePickerController.SourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = source
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+
 }
 
