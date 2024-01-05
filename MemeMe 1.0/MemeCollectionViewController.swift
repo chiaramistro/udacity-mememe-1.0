d2a5286ddc8c6860dd8ee4812ec4ad69f895f121
Chiara Mistrorigo <c.mistrorigo@reply.it>
2024-01-05 16:05:27 +0100

    Add view controller to collection view

---
+++ MemeMe 1.0/MemeCollectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Chiara Mistrorigo on 05/01/24.
+//
+
+import UIKit
+
+class MemeCollectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addMeme))
+    }
+    
+    @objc func addMeme() {
+        var controller: AddMemeViewController = self.storyboard?.instantiateViewController(withIdentifier: "AddMemeViewController") as! AddMemeViewController
+
+        present(controller, animated: true, completion: nil)
+    }
+    
+}
