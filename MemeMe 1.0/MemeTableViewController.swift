be9240e1f04fbee3d3d63994299b23dfe7981249
Chiara Mistrorigo <c.mistrorigo@reply.it>
2024-01-08 17:14:37 +0100

    Fix table view (adding UITableViewController)

--- MemeMe 1.0/MemeTableViewController.swift
+++ MemeMe 1.0/MemeTableViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
-class MemeTableViewController: UIViewController {
+class MemeTableViewController: UITableViewController {
+    
+    let memeArray: [String] = [
+        "Meme 1",
+        "Meme 2",
+        "Meme 3",
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,4 +26,22 @@ class MemeTableViewController: UIViewController {
         present(controller, animated: true, completion: nil)
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.memeArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeTableViewCell")!
+        let meme = self.memeArray[(indexPath as NSIndexPath).row]
+        
+        // Set the name
+        cell.textLabel?.text = meme
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Navigate on click item \((indexPath as NSIndexPath).row)")
+    }
+
 }
